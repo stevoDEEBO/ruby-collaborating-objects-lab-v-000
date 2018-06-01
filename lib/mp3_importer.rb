1696d125e12133e928194c5b4854650ebff17783
Stephen Telesmanic <steve.telesmanic@gmail.com>
2018-06-01 07:04:41 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -3,22 +3,19 @@ attr_accessor :path
 
   def initialize(path)
     @path = path
-    @mp3s = []
-    @mp3s_without_path = []
   end
 
   def files
     #@mp3s_without_path = []
     files = []
-    filename = []
+    @filenames = []
     files = Dir[self.path].each {|file| files << file}
     files.each {|file| filenames << file.split("mp3s/")[1]}
     filenames
   end
 
   def import
-    @mp3s_without_path.each do |filename|
-      Song.new_by_filename(filename)
+    self.files.each {|filename| Song.new_by_filename(filename)}
     end
   end
 end
